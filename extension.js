52c8247fd3ab86eb101cf80cd9435cf727b16df0
Sonic73 <sonic_t_h@yahoo.com>
2019-08-31 03:54:25 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -105,9 +105,9 @@
       songstats: true,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://github.com/basicBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://github.com/basicBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://github.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+        NSFW: "https://raw.githack.com/basicBot/custom/master/blacklists/NSFWlist.json",
+        OP: "https://raw.githack.com/basicBot/custom/master/blacklists/OPlist.json",
+        BANNED: "https://raw.githack.com/basicBot/custom/master/blacklists/BANNEDlist.json"
       }
     }));
 
